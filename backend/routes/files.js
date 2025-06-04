caf90cdb671464e685dabd60f8fb57cae78cdfc7
OwenS3881 <owenski08@gmail.com>
2025-06-04 12:28:28 +0200

    Protected delete files route for admins

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const filesController = require("../controllers/filesController");
 const multer = require("multer");
+const { adminAuthMiddleware } = require("../middleware/auth");
 
 //configure local storage
 const storage = multer.memoryStorage();
@@ -10,7 +11,7 @@ const upload = multer({ storage });
 router.post("/upload", upload.single("file"), filesController.handleUpload);
 router.get("/list", filesController.listFiles);
 router.post("/:id", filesController.getFileById);
-router.delete("/delete/:id", filesController.deleteFile);
+router.delete("/delete/:id", adminAuthMiddleware, filesController.deleteFile);
 router.post("/folder/create", filesController.createFolder);
 
 module.exports = router;
