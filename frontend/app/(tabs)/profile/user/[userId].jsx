cbe041e9cd27b7decb2108d77d5f53103185a45f
OwenS3881 <owenski08@gmail.com>
2025-06-05 12:32:28 +0200

    Resolved conflicts with removing users

--- frontend/app/(tabs)/profile/user/[userId].jsx
+++ frontend/app/(tabs)/profile/user/[userId].jsx
@@ -74,7 +74,7 @@ const UserPage = () => {
     const removeUser = async () => {
         try {
             showLoading();
-            await API.put(`/admin/remove/${user._id}`);
+            await API.put(`/admin/removeid/${user._id}`);
         } catch (err) {
             console.log("Error deleting user", err);
         } finally {
